429afc816a38e4c1b018c71b195a0654bc2c04f1
Mferik <mfatiherikk@gmail.com>
2023-05-10 12:55:33 +0300

    config ayarları düzeltildi

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-module_exports = {
+module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "shhh",
